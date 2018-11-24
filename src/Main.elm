b61f29db6fa3b78fccf6ff788dadd5dcaf4bcd72
YuyaAizawa <yaizawa@sa.cs.titech.ac.jp>
2018-11-24 15:33:43 +0900

    旗を立てられるように

--- src/Main.elm
+++ src/Main.elm
@@ -3,8 +3,8 @@ import List exposing (..)
 import Maybe exposing (withDefault)
 import Random exposing (initialSeed, step)
 import Random.List exposing (shuffle)
-import Html exposing (Html, table, tbody, tr, td, input, div, text)
-import Html.Attributes exposing (disabled, type_, value)
+import Html exposing (Html, table, tbody, tr, td, input, div, text, fieldset, label)
+import Html.Attributes exposing (disabled, type_, value, name)
 import Html.Events exposing (onClick, onInput)
 
 
@@ -17,12 +17,18 @@ main =
 type alias Model =
   { field : List (List FieldStatus)
   , nextSeed : Maybe Int
+  , clickTo : ClickTo
   }
 
 type alias FieldStatus =
-  { opened : Bool
+  { surface : Surface
   , underground : Underground
   }
+
+type Surface
+  = Covered
+  | Uncovered
+  | Flagged
 
 type Underground
   = Mine
@@ -36,6 +42,7 @@ init =
 
   { field = newField width height mines (initialSeed 0)
   , nextSeed =  Nothing
+  , clickTo = Dig
   }
 
 newField : Int -> Int -> Int -> Random.Seed -> List (List FieldStatus)
@@ -68,14 +75,14 @@ newField width height mines seed =
       |> map (\y -> range 0 (width - 1)
         |> map (\x ->
           if member (x, y) minedCoords
-          then FieldStatus False Mine
+          then FieldStatus Covered Mine
           else
             let
               num = neighbor (x, y)
                 |> map (\c -> if member c minedCoords then 1 else 0)
                 |> sum
             in
-            FieldStatus False (Empty num)
+            FieldStatus Covered (Empty num)
         )
       )
 
@@ -85,12 +92,23 @@ type Msg
   = Reset
   | Open Coords
   | Seed String
+  | ClickMode ClickTo
 
 type alias Coords = (Int, Int)
+
+type ClickTo
+  = Dig
+  | Flag
 
 update : Msg -> Model -> Model
 update msg model =
   let _ = Debug.log "msg" <| msgToString msg in
+  let
+    updateField coords updater field =
+      field |> indexed2Map (\x -> \y -> \f -> if (coords == (x, y))
+        then updater f
+        else f)
+  in
   case msg of
     Reset -> case model.nextSeed of
       Nothing -> model
@@ -102,21 +120,35 @@ update msg model =
           newField width height mines (initialSeed seed)
         }
 
-    Open coords ->
-      { model | field =
-        model.field
-          |> indexed2Map (\x -> \y -> \u -> if coords == (x, y)
-            then {opened = True, underground = u.underground}
-            else u)
-      }
+    Open coords -> case model.clickTo of
+      Dig ->
+        { model | field =
+          model.field
+            |> updateField coords (\f -> if f.surface == Covered
+              then {f | surface = Uncovered}
+              else f)
+        }
+      Flag ->
+        { model | field =
+          model.field
+            |> updateField coords (\f -> case f.surface of
+              Covered -> {f | surface = Flagged}
+              Flagged -> {f | surface = Covered}
+              Uncovered -> f)
+        }
 
     Seed seed -> { model | nextSeed = seed |> String.toInt }
+
+    ClickMode mode -> { model | clickTo = mode }
 
 msgToString : Msg -> String
 msgToString msg = case msg of
   Reset -> "Reset"
   Open coords -> coords |> coordsToString
   Seed str -> "Input " ++ str
+  ClickMode mode -> "ClickTo " ++ case mode of
+    Dig -> "Dig"
+    Flag -> "Flag"
 
 coordsToString : Coords -> String
 coordsToString (x, y) =
@@ -148,6 +180,10 @@ view model =
     , table []
       [ tbody [] <| mineField model
       ]
+    , viewPicker "clickTo"
+      [ ("Dig" ,ClickMode Dig)
+      , ("Flag", ClickMode Flag)
+      ]
     ]
 
 mineField : Model -> List (Html Msg)
@@ -159,13 +195,26 @@ mineField model =
 coordButton : Coords -> FieldStatus -> Html Msg
 coordButton coords field =
   let
-    txt = case (field.opened, field.underground) of
-      (True, Mine) -> "\u{1F4A3}"
-      (True, Empty i) -> i |> String.fromInt
-      (False, _) -> coordsToString coords
+    txt = case field.surface of
+      Covered -> coordsToString coords
+      Flagged -> "\u{1F6A9}"
+      Uncovered -> case field.underground of
+        Mine -> "\u{1F4A3}"
+        Empty i -> i |> String.fromInt
   in
-    input [
-      type_ "button",
-      onClick <| Open coords,
-      disabled field.opened,
-      value txt][]
+  input [
+    type_ "button",
+    onClick <| Open coords,
+    disabled <| field.surface == Uncovered,
+    value txt][]
+
+viewPicker : String -> List (String, Msg) -> Html Msg
+viewPicker group options =
+  fieldset [] (options |> map (radio group))
+
+radio : String -> (String, Msg) -> Html Msg
+radio group (string, msg) =
+  label []
+    [ input [ type_ "radio", name group, onClick msg ] []
+    , text string
+    ]
